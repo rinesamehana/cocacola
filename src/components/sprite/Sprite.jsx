ad86956ab408c6be7ad13ca225fe5a84f677f33a
rinesamehana <93328298+rinesamehana@users.noreply.github.com>
2023-05-29 22:42:28 +0200

    sprite page added animations

--- src/components/sprite/Sprite.jsx
+++ src/components/sprite/Sprite.jsx
@@ -1,22 +1,42 @@
-import React from 'react';
-import './sprite.css';
-import { Carousel } from 'react-responsive-carousel';
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import React from "react";
+import "./sprite.css";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const Sprite = () => {
   return (
     <>
-  <div class="container">
-  <h2 class="title">
-    <span class="title-word title-word-1">OUR</span>
-    <span class="title-word title-word-2">COMPANY</span>
-    <span class="title-word title-word-3">COCA</span>
-    <span class="title-word title-word-4">COLA</span>
-  </h2>
-</div>
+      <section >
+        <div class="main">
+          <div class="text">
+            <h1>
+              THAT'S WHAT<span>I LIKE</span>
+            </h1>
+            <p>
+              Sprite is a clear, lemon and lime-flavored soft drink created by
+              the Coca-Cola Company. Sprite comes in multiple flavors, including
+              cranberry, cherry, grape, orange, tropical, ginger, and vanilla.
+              Ice, peach, Berryclear remix, and newer versions of the drinks are
+              artificially sweetened. Sprite was created to compete primarily
+              against 7 Up.
+            </p>
 
+            
+            <div class="bounce-in-back">
+            <a href="#" class="btn">
+              COCA COLA BRAND
+            </a>
+            </div>
 
+          </div>
 
+          <div class="image">
+            <img src="../../../images/5.png" />
+          </div>
+
+         
+        </div>
+      </section>
     </>
   );
 };
