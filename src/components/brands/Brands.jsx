1216ce40787b55224d0a5c10727fc83a1da0cc53
rinesamehana <93328298+rinesamehana@users.noreply.github.com>
2023-05-29 18:21:44 +0200

    update brands animation

--- src/components/brands/Brands.jsx
+++ src/components/brands/Brands.jsx
@@ -4,23 +4,68 @@ import './brands.css';
 const Brands = () => {
   return (
    <>
+   <div class="container">
    <div class="line">
    <img src="https://www.freepnglogos.com/uploads/sprite-png-logo/sprite-lemon-lime-soda-png-logo-4.png" class='flipX'/>
 </div>
 
   <div class="line">
-    <img src="https://www.freepnglogos.com/uploads/sprite-png-logo/sprite-lemon-lime-soda-png-logo-4.png" class='lineUp'/>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/5/59/Fanta_logo_%282018%29.png" class='lineUp'/>
   </div>
 
   <div class='line'>
-  <img src="https://www.freepnglogos.com/uploads/sprite-png-logo/sprite-lemon-lime-soda-png-logo-4.png" class='flipY'/>
+  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/Diet_Coke_Logo_1997.svg/1200px-Diet_Coke_Logo_1997.svg.png" class='flipY'/>
   </div>
 
 
   <div class='line'>
   <img src="https://www.freepnglogos.com/uploads/sprite-png-logo/sprite-lemon-lime-soda-png-logo-4.png" class='pop-outin'/>
   </div>
-   
+
+  </div>
+
+
+  <div class="container">
+   <div class="line">
+   <img src="https://www.freepnglogos.com/uploads/sprite-png-logo/sprite-lemon-lime-soda-png-logo-4.png" class='flipX'/>
+</div>
+
+  <div class="line">
+    <img src="https://upload.wikimedia.org/wikipedia/commons/5/59/Fanta_logo_%282018%29.png" class='lineUp'/>
+  </div>
+
+  <div class='line'>
+  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/Diet_Coke_Logo_1997.svg/1200px-Diet_Coke_Logo_1997.svg.png" class='flipY'/>
+  </div>
+
+
+  <div class='line'>
+  <img src="https://www.freepnglogos.com/uploads/sprite-png-logo/sprite-lemon-lime-soda-png-logo-4.png" class='pop-outin'/>
+  </div>
+
+  </div>
+
+
+
+  <div class="container">
+   <div class="line">
+   <img src="https://www.freepnglogos.com/uploads/sprite-png-logo/sprite-lemon-lime-soda-png-logo-4.png" class='flipX'/>
+</div>
+
+  <div class="line">
+    <img src="https://upload.wikimedia.org/wikipedia/commons/5/59/Fanta_logo_%282018%29.png" class='lineUp'/>
+  </div>
+
+  <div class='line'>
+  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/Diet_Coke_Logo_1997.svg/1200px-Diet_Coke_Logo_1997.svg.png" class='flipY'/>
+  </div>
+
+
+  <div class='line'>
+  <img src="https://www.freepnglogos.com/uploads/sprite-png-logo/sprite-lemon-lime-soda-png-logo-4.png" class='pop-outin'/>
+  </div>
+
+  </div>
    
    
    </>
