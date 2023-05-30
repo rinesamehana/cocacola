8e625120a165d0afdc7cbe6e71b0206255975236
rinesamehana <93136623+AdiFejza@users.noreply.github.com>
2023-05-30 12:55:46 +0200

    adding audio effects

--- src/components/fuzetea/Fuzetea.jsx
+++ src/components/fuzetea/Fuzetea.jsx
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import './fuzetea.css';
 
-
 const Fuzetea = () => {
+  const audioRef = useRef(null);
+
+  useEffect(() => {
+    const audioElement = audioRef.current;
+
+    const playAudio = () => {
+      audioElement.play();
+    };
+
+    // Play audio automatically when the component mounts
+    playAudio();
+
+    // Clean up function
+    return () => {
+      audioElement.pause();
+      audioElement.currentTime = 0;
+    };
+  }, []);
+
   return (
-    <>
-           <div>Hello </div>
-    </>
+    <div className="fuzetea-container" transition-style="in:circle:top-right">
+      <div className="fuzetea-bubble">
+        <img src="../../images/fuzetea.png" alt="Fuzetea Logo" className="fuzetea-logo" />
+      </div>
+      <audio ref={audioRef} src="../../sounds/fuzetea.wav" autoPlay />
+      <h1>Welcome to Fuzetea!</h1>
+      {/* Other attractive content */}
+    </div>
   );
 };
 
--- src/components/layout/navbar/Navbar.jsx
+++ src/components/layout/navbar/Navbar.jsx
@@ -20,6 +20,12 @@ function Navbar() {
     if (pathname === "/spritepage") {
       return "active-sprite";
     } 
+    if (pathname === "/fuzeteapage") {
+      return "active-fuzetea";
+    } 
+    if (pathname === "/schweppespage") {
+      return "active-schweppes";
+    } 
     
     return ""; // Default class when not on a specific page
   };
