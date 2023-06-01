79587d0c77b4da3f29904fabc79d0f47f06f446f
rinesamehana <93328298+rinesamehana@users.noreply.github.com>
2023-06-02 00:25:04 +0200

    update home

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -4,40 +4,48 @@ import { Controller, Scene } from 'react-scrollmagic';
 import './home.css';
 
 const Home = () => {
-
   return (
     <>
-     <Controller>
-    <div className="video-section">
-    <video autoPlay loop muted className="video">
-      <source src="../../../videos/intro.mp4" type="video/mp4" />
-    </video>
-  </div>
-   
+      <Controller>
+        <Scene duration={500} triggerElement=".video-section">
+          {(progress) => (
+            <div className="video-section">
+              <video autoPlay loop muted className="video">
+                <source src="../../../videos/intro.mp4" type="video/mp4" />
+              </video>
+            </div>
+          )}
+        </Scene>
 
-  <div className="section">
-      <div className="image-container">
-        <img src="../../../images/cocacoke.webp" alt="Image" />
-      </div>
+        <Scene duration={500} triggerElement=".section">
+          {(progress) => (
+            <div className="section">
+              <div className="image-container">
+                <img src="../../../images/cocacoke.webp" alt="Image" />
+              </div>
 
-      <div className="card-text">
-             <h2>A Purpose-Driven History</h2>
-           <p>
-           The Coca‑Cola Company’s purpose is to refresh the world and make a difference and we have remained true to that purpose for 137 years.
-           </p>
-      </div>
+              <div className="card-text">
+                <h2>A Purpose-Driven History</h2>
+                <p>
+                  The Coca‑Cola Company’s purpose is to refresh the world and make a difference and we have remained true to that purpose for 137 years.
+                </p>
+              </div>
+            </div>
+          )}
+        </Scene>
 
-    </div>
-    <div className="picture-frame">
-      <div className="image-container">
-        <h2 className="title">Everywhere</h2>
-      </div>
-    </div>
-    </Controller>
-
-  </>
+        <Scene duration={300} triggerElement=".picture-frame">
+          {(progress) => (
+            <div className="picture-frame">
+              <div className="image-container">
+                <h2 className="title">Everywhere</h2>
+              </div>
+            </div>
+          )}
+        </Scene>
+      </Controller>
+    </>
   );
 };
 
 export default Home;
-
