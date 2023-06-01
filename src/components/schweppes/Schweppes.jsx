1e6b67f733cf56bc1f08c5c41da5b565e5d4fcd2
rinesamehana <93328298+rinesamehana@users.noreply.github.com>
2023-06-02 01:57:32 +0200

    update schweppes

--- src/components/schweppes/Schweppes.jsx
+++ src/components/schweppes/Schweppes.jsx
@@ -4,13 +4,28 @@ import "./schweppes.css";
 const Schweppes = () => {
   return (
     <>
-   
-   <div className="video-section">
+      <div className="video-section">
         <video autoPlay loop className="video">
           <source src="../../../videos/schweppes.mp4" type="video/mp4" />
         </video>
       </div>
 
+      <div className="section">
+        <div className="container">
+          <div className="textbox">
+            <h2>Our purpose is to refresh the world and make a difference</h2>
+            <p>
+              We are committed to offering people more of the drinks they want
+              across a range of categories and sizes while driving sustainable
+              solutions that build resilience into our business and create
+              positive change for the planet.
+            </p>
+          </div>
+          <div className="imgbox">
+            <img src="../../images/img.jpg" alt="Image description" />
+          </div>
+        </div>
+      </div>
     </>
   );
 };
