db82ef1a209b0933b312bbae91c6dd2551daaa42
rinesamehana <93328298+rinesamehana@users.noreply.github.com>
2023-06-02 01:27:26 +0200

    remove

--- src/components/sprite/Sprite.jsx
+++ src/components/sprite/Sprite.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import "./sprite.css";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import ScrollAnimation from "react-animate-on-scroll";
 
 const Sprite = () => {
   return (
@@ -16,34 +15,28 @@ const Sprite = () => {
       <section>
         <div className="main">
           <div className="text">
-            <ScrollAnimation animateIn="fadeInUp" animateOnce>
-              <h1>
-                THAT'S WHAT<span>I LIKE</span>
-              </h1>
-              <p>
-                Sprite is a clear, lemon and lime-flavored soft drink created by
-                the Coca-Cola Company. Sprite comes in multiple flavors, including
-                cranberry, cherry, grape, orange, tropical, ginger, and vanilla.
-                Ice, peach, Berryclear remix, and newer versions of the drinks are
-                artificially sweetened. Sprite was created to compete primarily
-                against 7 Up.
-              </p>
-            </ScrollAnimation>
+            <h1>
+              THAT'S WHAT<span>I LIKE</span>
+            </h1>
+            <p>
+              Sprite is a clear, lemon and lime-flavored soft drink created by
+              the Coca-Cola Company. Sprite comes in multiple flavors, including
+              cranberry, cherry, grape, orange, tropical, ginger, and vanilla.
+              Ice, peach, Berryclear remix, and newer versions of the drinks are
+              artificially sweetened. Sprite was created to compete primarily
+              against 7 Up.
+            </p>
 
             <div className="bounce-in-back">
-              <ScrollAnimation animateIn="fadeInUp" animateOnce>
-                <a href="#" className="btn">
-                  COCA COLA BRAND
-                </a>
-              </ScrollAnimation>
+              <a href="#" className="btn">
+                COCA COLA BRAND
+              </a>
             </div>
           </div>
 
-          <ScrollAnimation animateIn="fadeInRight" animateOnce>
-            <div className="image">
-              <img src="../../../images/5.png" alt="Sprite" />
-            </div>
-          </ScrollAnimation>
+          <div className="image">
+            <img src="../../../images/5.png" alt="Sprite" />
+          </div>
         </div>
       </section>
     </>
