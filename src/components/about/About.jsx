da5a191f12f6c0754f3a762fb2cc216222fd3488
rinesamehana <93328298+rinesamehana@users.noreply.github.com>
2023-06-02 02:20:37 +0200

    update schweppes

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,58 +1,67 @@
-import React from 'react';
-import './about.css';
-import { Carousel } from 'react-responsive-carousel';
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import React from "react";
+import "./about.css";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const About = () => {
   return (
     <>
-  <div class="container">
-  <h2 class="title">
-    <span class="title-word title-word-1">OUR</span>
-    <span class="title-word title-word-2">COMPANY</span>
-    <span class="title-word title-word-3">COCA</span>
-    <span class="title-word title-word-4">COLA</span>
-  </h2>
-</div>
-
-
-
-
-<div className="video-section">
-    <video autoPlay loop muted className="video">
-      <source src="../../../videos/company.mp4" type="video/mp4" />
-    </video>
-  </div>
-
-
-  <div className="section">
-      <div className="text-box">
-        <h2>Our purpose is to refresh the world and make a difference</h2>
-        <p>We are committed to offering people more of the drinks they want across a range of categories 
-          and and sizes while driving sustainable solutions that build resilience into our business and create positive change for the planet..</p>
+    <div class="about">
+      <div class="container">
+        <h2 class="title">
+          <span class="title-word title-word-1">OUR</span>
+          <span class="title-word title-word-2">COMPANY</span>
+          <span class="title-word title-word-3">COCA</span>
+          <span class="title-word title-word-4">COLA</span>
+        </h2>
       </div>
-      <div className="slider-box">
-        <Carousel autoPlay interval={2000} infiniteLoop showStatus={false} showThumbs={false}>
-          <div>
-            <img src="https://www.coca-colacompany.com/content/dam/journey/au/en/our-company/Our%20Company%20Module%20Suggestion.png" alt="Slide 1" />
-          </div>
-          <div>
-            <img src="https://cdn.trendhunterstatic.com/thumbs/303/cocacola-head-office.jpeg?auto=webp" alt="Slide 2" />
-          </div>
-          <div>
-            <img src="https://cocacolaunited-old.s3.amazonaws.com/wp-content/uploads/2013/06/Milledgeville-100-Year.jpg" alt="Slide 3" />
-          </div>
-        </Carousel>
+
+      <div className="video-section">
+        <video autoPlay loop muted className="video">
+          <source src="../../../videos/company.mp4" type="video/mp4" />
+        </video>
       </div>
-    </div>
-
-
-
-
-
-
-
-
+      
+      <div className="section">
+        <div className="text-box">
+          <h2>Our purpose is to refresh the world and make a difference</h2>
+          <p>
+            We are committed to offering people more of the drinks they want
+            across a range of categories and and sizes while driving sustainable
+            solutions that build resilience into our business and create
+            positive change for the planet..
+          </p>
+        </div>
+        <div className="slider-box">
+          <Carousel
+            autoPlay
+            interval={2000}
+            infiniteLoop
+            showStatus={false}
+            showThumbs={false}
+          >
+            <div>
+              <img
+                src="https://www.coca-colacompany.com/content/dam/journey/au/en/our-company/Our%20Company%20Module%20Suggestion.png"
+                alt="Slide 1"
+              />
+            </div>
+            <div>
+              <img
+                src="https://cdn.trendhunterstatic.com/thumbs/303/cocacola-head-office.jpeg?auto=webp"
+                alt="Slide 2"
+              />
+            </div>
+            <div>
+              <img
+                src="https://cocacolaunited-old.s3.amazonaws.com/wp-content/uploads/2013/06/Milledgeville-100-Year.jpg"
+                alt="Slide 3"
+              />
+            </div>
+          </Carousel>
+        </div>
+      </div>
+      </div>
     </>
   );
 };
--- src/components/schweppes/Schweppes.jsx
+++ src/components/schweppes/Schweppes.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./schweppes.css";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const Schweppes = () => {
   return (
@@ -13,7 +15,7 @@ const Schweppes = () => {
       <div className="section">
         <div className="container">
           <div className="textbox">
-            <h2>Our purpose is to refresh the world and make a difference</h2>
+            <h2>Enjoy this sturdy drink with some sweet people! 🙌  </h2>
             <p>
               We are committed to offering people more of the drinks they want
               across a range of categories and sizes while driving sustainable
@@ -22,9 +24,29 @@ const Schweppes = () => {
             </p>
           </div>
           <div className="imgbox">
-            <img src="../../images/img.jpg" alt="Image description" />
+            <Carousel
+              autoPlay
+              interval={2000}
+              infiniteLoop
+              showStatus={false}
+              showThumbs={false}
+            >
+              <div>
+                <img src="../../images/img.jpg" alt="Slide 1" />
+              </div>
+              <div>
+                <img src="../../images/img1.jpg" alt="Slide 2" />
+              </div>
+              <div>
+                <img src="../../images/img2.jpg" alt="Slide 3" />
+              </div>
+            </Carousel>
           </div>
         </div>
+      </div>
+
+      <div class="cover-section">
+        <img src="../../images/img3.jpg" alt="Cover Image" />
       </div>
     </>
   );
