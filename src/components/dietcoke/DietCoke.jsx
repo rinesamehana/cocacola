7f2139613c1dce9d486e0843652e233ba77198a2
rinesamehana <93136623+AdiFejza@users.noreply.github.com>
2023-06-02 18:50:09 +0200

    adding pixijs

--- src/components/dietcoke/DietCoke.jsx
+++ src/components/dietcoke/DietCoke.jsx
@@ -1,11 +1,35 @@
 import React, { useEffect } from 'react';
+import * as PIXI from 'pixi.js';
+import './dietcoke.css'; // Import the CSS file for styling
 
 const DietCoke = () => {
- 
+  useEffect(() => {
+    const app = new PIXI.Application({ backgroundAlpha: 0 });
+    document.body.appendChild(app.view);
+
+    const bunny = PIXI.Sprite.from('https://images.albertsons-media.com/is/image/ABS/960026405?$ng-ecom-pdp-desktop$&defaultImage=Not_Available');
+    bunny.anchor.set(0.5);
+    bunny.x = app.screen.width / 2;
+    bunny.y = app.screen.height / 2;
+
+    const rotationSpeed = 0.05; // Adjust the speed of rotation (higher value = faster)
+
+    app.stage.addChild(bunny);
+
+    app.ticker.add(() => {
+      bunny.rotation += rotationSpeed;
+    });
+
+    return () => {
+      app.destroy();
+    };
+  }, []);
+
+
 
   return (
-    <div>
-      <h1>Diet Coke Page</h1>
+    <div className="diet-coke-container">
+      <h1>D</h1>
     </div>
   );
 };
