4a0e9591453f4e6bf8971d37597ea6fc0bbb5240
AdiFejza <93136623+AdiFejza@users.noreply.github.com>
2023-06-03 15:00:20 +0200

    Fixed animations

--- src/components/brands/Brands.jsx
+++ src/components/brands/Brands.jsx
@@ -9,7 +9,7 @@ const Brands = () => {
    <div class="container">
    <div class="line">
    <NavLink to="/spritepage">
-   <img src="https://www.freepnglogos.com/uploads/sprite-png-logo/sprite-lemon-lime-soda-png-logo-4.png" class='flipX'/>
+   <img src="https://www.freepnglogos.com/uploads/sprite-png-logo/sprite-lemon-lime-soda-png-logo-4.png" class='lineUp'/>
    </NavLink>
 </div>
 
@@ -21,14 +21,14 @@ const Brands = () => {
 
   <div class='line'>
   <NavLink to="/schweppespage">
-  <img src="https://upload.wikimedia.org/wikipedia/commons/0/09/Schweppes_Logo_2016.png" class='flipY'/>
+  <img src="https://upload.wikimedia.org/wikipedia/commons/0/09/Schweppes_Logo_2016.png" class='lineUp'/>
   </NavLink>
   </div>
 
 
   <div class='line'>
   <NavLink to="/fuzeteapage">
-  <img src="https://www.coca-cola-france.fr/content/dam/one/fr/fr/logo-color/fuzetea-logo.png" class='pop-outin'/>
+  <img src="https://www.coca-cola-france.fr/content/dam/one/fr/fr/logo-color/fuzetea-logo.png" class='lineUp'/>
   </NavLink>
   </div>
 
@@ -37,7 +37,7 @@ const Brands = () => {
 
   <div class="container">
    <div class="line">
-   <img src="https://seeklogo.com/images/A/Ades-logo-7677395FC3-seeklogo.com.png" class='flipX'/>
+   <img src="https://seeklogo.com/images/A/Ades-logo-7677395FC3-seeklogo.com.png" class='lineUp'/>
 </div>
 
   <div class="line">
@@ -46,13 +46,13 @@ const Brands = () => {
 
   <div class='line'>
   <NavLink to="/dietcokepage">
-  <img src="../../images/dietcokee.png" class='flipY'/>
+  <img src="../../images/dietcokee.png" class='lineUp'/>
   </NavLink>
   </div>
 
 
   <div class='line'>
-  <img src="https://upload.wikimedia.org/wikipedia/fr/thumb/d/d9/Logo_Glac%C3%A9au_vitaminwater.png/1200px-Logo_Glac%C3%A9au_vitaminwater.png" class='pop-outin'/>
+  <img src="https://upload.wikimedia.org/wikipedia/fr/thumb/d/d9/Logo_Glac%C3%A9au_vitaminwater.png/1200px-Logo_Glac%C3%A9au_vitaminwater.png" class='lineUp'/>
   </div>
 
   </div>
